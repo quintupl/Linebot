dfc5959a9a8ba0be333dc24cf599965df6e88611
quintupl <134512993+quintupl@users.noreply.github.com>
2025-02-21 22:36:17 +0900

    Update MapNow.java

--- src/main/java/com/example/linebot/replier/MapNow.java
+++ src/main/java/com/example/linebot/replier/MapNow.java
@@ -6,6 +6,7 @@ import org.apache.coyote.BadRequestException;
 
 import java.util.ArrayList;
 
+// 送信された位置情報から緯度と経度を取得するクラス
 public class MapNow {
     private MessageEvent<LocationMessageContent> event;
     private double latitude;
@@ -16,9 +17,11 @@ public class MapNow {
     }
 
     public ArrayList<Double> getCoordinate() throws BadRequestException {
+        // 緯度と経度の値を持つArrayListの作成
         ArrayList<Double> coordinate = new ArrayList<>();
         this.latitude = this.event.getMessage().getLatitude();
         this.longitude = this.event.getMessage().getLongitude();
+        //
         if(event.getMessage().getAddress().isEmpty()) {
             throw new BadRequestException();
         }
